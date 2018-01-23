f8c59d107d6d8201e7fbd92a2327c05e9e49ade0
Dave Snowberger <git@fartherout.org>
2018-01-22 16:35:56 -0800

    added some files

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+//Requires
+const express = require('express');
+const pg = require('pg');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+//Additional consts
+const app = express();
+const PORT = process.env.PORT || 3000;
+const conString = process.env.DATABASE_URL || database;
+const client = new pg.Client(consString);
+
+//connect to db
+client.connect();
+
+//set up body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true});
